1be5a7c513df46d382deb69792193d47968d6cff
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-17 03:09:45 +0900

    feat: Header 컴포넌트 생성
    - 다크모드 전환

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import classnames from 'classnames/bind';
+import { IoIosSunny, IoMdMoon } from 'react-icons/io';
+import { useDarkMode } from 'src/context/DarkModeContext';
+import styles from './Header.module.css';
+const cx = classnames.bind(styles);
+
+const Header = () => {
+    const { darkMode, toggleDarkMode } = useDarkMode();
+    return (
+        <header id={cx('header')}>
+            <div className={cx('header')}>
+                <h1 className={cx('header-logo')}>DeBus</h1>
+                <div className={cx('header-right')}>
+                    <button className={cx('btn-mode')} onClick={toggleDarkMode}>
+                        {darkMode ? <IoIosSunny /> : <IoMdMoon />}
+                    </button>
+                    <button className={cx('btn-login')}>로그인</button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
