0ce461c7e788e7bf54a034f27e60321421f29406
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-19 15:36:12 +0900

    refactor: useTheme 커스텀훅 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,14 @@ import styles from './Header.module.css';
 const cx = classnames.bind(styles);
 
 const Header = () => {
-    const { darkMode, toggleDarkMode } = useDarkMode();
+    const [darkMode, toggleDarkMode] = useDarkMode();
+    const handleToggle = () => toggleDarkMode();
     return (
         <header id={cx('header')}>
             <div className={cx('header')}>
                 <h1 className={cx('header-logo')}>DeBus</h1>
                 <div className={cx('header-right')}>
-                    <button className={cx('btn-mode')} onClick={toggleDarkMode}>
+                    <button className={cx('btn-mode')} onClick={handleToggle}>
                         {darkMode ? <IoMdMoon /> : <IoIosSunny />}
                     </button>
                     <button className={cx('btn-login')}>로그인</button>
--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,32 +1,20 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { DarkModeState, useTheme } from '@hooks/useTheme';
+import { PropsWithChildren, createContext, useContext, useEffect } from 'react';
 
-type State = {
-    darkMode: boolean;
-    toggleDarkMode: () => void;
-};
+const DarkModeContext = createContext<DarkModeState | null>(null);
 
-const DarkModeContext = createContext<State | null>(null);
-
-export const DarkModeProvider = ({
-    children,
-}: {
-    children: React.ReactNode;
-}) => {
-    const [darkMode, setDarkMode] = useState(false);
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        updateDarkMode(!darkMode);
-    };
+export const DarkModeProvider = ({ children }: PropsWithChildren) => {
+    const [darkMode, toggleDarkMode] = useTheme(false);
     useEffect(() => {
         const isDark =
             localStorage.theme === 'dark' ||
             (!('theme' in localStorage) &&
                 window.matchMedia('(prefers-color-scheme: dark)').matches);
-        setDarkMode(isDark);
-        updateDarkMode(isDark);
-    }, []);
+        toggleDarkMode(isDark);
+    }, [toggleDarkMode]);
+
     return (
-        <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+        <DarkModeContext.Provider value={[darkMode, toggleDarkMode]}>
             {children}
         </DarkModeContext.Provider>
     );
@@ -36,14 +24,4 @@ export const useDarkMode = () => {
     const state = useContext(DarkModeContext);
     if (!state) throw new Error('Cannot find DarkModeProvider');
     return state;
-};
-
-const updateDarkMode = (darkMode: boolean) => {
-    if (darkMode) {
-        document.documentElement.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
 };
