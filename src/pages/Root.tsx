c3e3a8f2b5ca61ad48015e63bbf1db53c4d56b8a
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-21 01:34:34 +0900

    #3 refactor: Context Provider 코드 제거

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,16 +1,15 @@
 import Header from '@components/Header/Header';
 import RootContainer from '@components/UI/RootContainer/RootContainer';
 import { Outlet } from 'react-router-dom';
-import { DarkModeProvider } from 'src/context/DarkModeContext';
 
 const Root = () => {
     return (
-        <DarkModeProvider>
+        <>
             <Header />
             <RootContainer>
                 <Outlet />
             </RootContainer>
-        </DarkModeProvider>
+        </>
     );
 };
 
