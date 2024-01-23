b462ff06ffe97d8a21c5904d114c743b1c6b335f
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-23 21:32:08 +0900

    #3 refactor: base atom을 atomWithStorage로 변경
    - App 컴포넌트: localStorage 테마 정보 조회 및 스타일 변경 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { darkMode_atom } from '@store/atoms/theme';
-import { useSetAtom } from 'jotai';
-import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'src/router';
 
 function App() {
-    const setDarkMode = useSetAtom(darkMode_atom);
-    useEffect(() => {
-        const isDark =
-            localStorage.theme === 'dark' ||
-            (!('theme' in localStorage) &&
-                window.matchMedia('(prefers-color-scheme: dark)').matches);
-        setDarkMode(isDark);
-    }, [setDarkMode]);
     return <RouterProvider router={router} />;
 }
 
--- src/store/atoms/theme.ts
+++ src/store/atoms/theme.ts
@@ -1,20 +1,10 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 
-export const darkMode_base_atom = atom(false);
+export const darkMode_storage_atom = atomWithStorage('darkMode', false);
 export const darkMode_atom = atom(
-    (get) => get(darkMode_base_atom),
+    (get) => get(darkMode_storage_atom),
     (_, set, darkMode: boolean) => {
-        set(darkMode_base_atom, darkMode);
-        updateDarkMode(darkMode);
+        set(darkMode_storage_atom, darkMode);
     },
 );
-
-const updateDarkMode = (darkMode: boolean) => {
-    if (darkMode) {
-        document.documentElement.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
-};
