9710263e6d5efa326c7e4a4224dee1a40f7d62bb
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-23 21:33:54 +0900

    #3 refactor: 분리한 테마 버튼 컴포넌트 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,15 @@
-import { darkMode_atom } from '@store/atoms/theme';
+import ThemeButton from '@components/UI/ThemeButton/ThemeButton';
 import classnames from 'classnames/bind';
-import { useAtom } from 'jotai';
-import { IoIosSunny, IoMdMoon } from 'react-icons/io';
 import styles from './Header.module.css';
 const cx = classnames.bind(styles);
 
 const Header = () => {
-    const [darkMode, setDarkMode] = useAtom(darkMode_atom);
-    const handleToggle = () => setDarkMode(!darkMode);
     return (
         <header id={cx('header')}>
             <div className={cx('header')}>
                 <h1 className={cx('header-logo')}>DeBus</h1>
                 <div className={cx('header-right')}>
-                    <button className={cx('btn-mode')} onClick={handleToggle}>
-                        {darkMode ? <IoMdMoon /> : <IoIosSunny />}
-                    </button>
+                    <ThemeButton />
                     <button className={cx('btn-login')}>로그인</button>
                 </div>
             </div>
