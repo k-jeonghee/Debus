a0a47bf94bb764610dc293f4f4036edfad3d9dc5
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-24 22:20:02 +0900

    #3 refactor: 렌더링 로직 컴포넌트 내부 선언으로 변경

--- src/components/UI/ThemeButton/ThemeButton.tsx
+++ src/components/UI/ThemeButton/ThemeButton.tsx
@@ -6,19 +6,19 @@ import { IoIosSunny, IoMdMoon } from 'react-icons/io';
 import styles from './ThemeButton.module.css';
 const cx = classnames.bind(styles);
 
-const toggleDarkClass = (darkMode: boolean) => {
-    if (darkMode) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-    }
-};
-
 const ThemeButton = () => {
     const [darkMode, setDarkMode] = useAtom(darkMode_atom);
     const handleTheme = () => {
         setDarkMode(!darkMode);
         toggleDarkClass(!darkMode);
+    };
+
+    const toggleDarkClass = (darkMode: boolean) => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
     };
 
     useEffect(() => {
