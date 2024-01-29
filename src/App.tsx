ef59e1f83a65155c20f315f799e7c5ad4560c2d3
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-01-29 22:14:24 +0900

    #3 refactor: 다크모드 스타일 적용 방법 dataset -> class로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Header from '@components/Header/Header';
-import { darkMode_atom } from '@store/atoms/theme';
+import { darkModeAtom } from '@store/atoms/theme';
 import { useAtomValue } from 'jotai';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-    const darkMode = useAtomValue(darkMode_atom);
+    const darkMode = useAtomValue(darkModeAtom);
     return (
-        <div id="app" data-theme={darkMode ? 'dark' : 'light'}>
+        <div id="app" className={darkMode ? 'dark' : 'light'}>
             <Header />
             <div className="container">
                 <Outlet />
