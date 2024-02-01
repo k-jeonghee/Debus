efb71c788b6aee6cb758d5c4b2fe1a85ecbe01f6
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-01 22:21:46 +0900

    #5 feat: Header 컴포넌트 로그인/로그아웃 상태 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,34 @@
+import Avatar from '@components/Avatar/Avatar';
 import ThemeButton from '@components/UI/ThemeButton/ThemeButton';
+import { authAtom } from '@store/atoms/auth';
 import classnames from 'classnames/bind';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { login, logout, onUserStateChange } from 'src/api/firebase';
 import styles from './Header.module.css';
 const cx = classnames.bind(styles);
 
 const Header = () => {
+    const [user, setUser] = useAtom(authAtom);
+    useEffect(() => {
+        onUserStateChange(setUser);
+    }, [setUser]);
     return (
         <header id={cx('header')}>
             <div className={cx('header')}>
                 <h1 className={cx('header-logo')}>DeBus</h1>
                 <div className={cx('header-right')}>
                     <ThemeButton />
-                    <button className={cx('btn-login')}>로그인</button>
+                    {user && <Avatar />}
+                    {user ? (
+                        <button className={cx('btn-login')} onClick={logout}>
+                            로그아웃
+                        </button>
+                    ) : (
+                        <button className={cx('btn-login')} onClick={login}>
+                            로그인
+                        </button>
+                    )}
                 </div>
             </div>
         </header>
