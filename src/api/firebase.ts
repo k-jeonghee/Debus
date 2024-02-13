1dc2d47f41938d6cbcacd31b11389a7b9a180f52
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-14 00:50:28 +0900

    feat: 데이터베이스에 사용자 정보 저장 로직 구현
    - 로그인 시 저장 여부 체크

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,12 +1,14 @@
+import { UserTypes } from '@store/atoms/auth';
 import { initializeApp } from 'firebase/app';
 import {
     GoogleAuthProvider,
-    User,
     getAuth,
     onAuthStateChanged,
     signInWithPopup,
     signOut,
 } from 'firebase/auth';
+import { get, getDatabase, ref, set } from 'firebase/database';
+import { assert } from 'src/utils/assert';
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -17,13 +19,23 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 const provider = new GoogleAuthProvider();
 
 export const login = async () => {
-    return signInWithPopup(auth, provider).catch((error) => {
-        throw new Error(error);
-    });
+    return signInWithPopup(auth, provider)
+        .then(async (res) => {
+            const user = res.user;
+            const isRegistered = await checkUserExists(user.uid);
+            if (!isRegistered)
+                createUser(user).catch((error) => {
+                    throw new Error(error);
+                });
+        })
+        .catch((error) => {
+            throw new Error(error);
+        });
 };
 
 export const logout = async () => {
@@ -32,5 +44,31 @@ export const logout = async () => {
     });
 };
 
-export const onUserStateChange = (callback: (user: User | null) => void) =>
-    onAuthStateChanged(auth, (user) => callback(user));
+export const onUserStateChange = (callback: (user: UserTypes | null) => void) =>
+    onAuthStateChanged(auth, async (user) => {
+        let data = {} as UserTypes;
+        if (user) {
+            data = {
+                uid: user.uid,
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+                email: user.email,
+            };
+        } else data = null;
+        callback(data);
+    });
+
+const checkUserExists = async (userId: string) => {
+    return get(ref(db, `users/${userId}`)).then((snapshot) =>
+        snapshot.exists(),
+    );
+};
+
+const createUser = (user: UserTypes) => {
+    assert(user !== null, '사용자가 없습니다.');
+    return set(ref(db, `users/${user.uid}`), {
+        name: user?.displayName,
+        email: user?.email,
+        photoURL: user?.photoURL,
+    });
+};
