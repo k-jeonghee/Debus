80ed304a9656e2a5bb55b3af6ffd130c860220e6
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-18 21:15:08 +0900

    #8 feat: ChatForm 기능 구현 및 스타일 추가

--- src/pages/Lines/ChatPanel/ChatForm/ChatForm.tsx
+++ src/pages/Lines/ChatPanel/ChatForm/ChatForm.tsx
@@ -1,18 +1,58 @@
 import classnames from 'classnames/bind';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 import { BsPlusCircleFill } from 'react-icons/bs';
 import { PiPaperPlaneTiltFill } from 'react-icons/pi';
 import styles from './ChatForm.module.css';
 const cx = classnames.bind(styles);
 
 const ChatForm = () => {
+    const [content, setContent] = useState('');
+    const [textareaHeight, setTextareaHeight] = useState('17px');
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(`${content} 전송!`);
+        setContent('');
+        setTextareaHeight('17px');
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const value = e.target.value;
+        setContent(value);
+        resizeTextareaHeight(value);
+    };
+
+    const resizeTextareaHeight = useCallback(
+        (value: string) => {
+            const lineHeight = 17;
+            const totalTextHeight = value.split('\n').length * lineHeight;
+            const newHeight = totalTextHeight < lineHeight ? textareaHeight : totalTextHeight;
+            setTextareaHeight(newHeight + 'px');
+        },
+        [textareaHeight],
+    );
+
     return (
         <div className={cx('container')}>
             <div className={cx('inner-form-container')}>
-                <form>ChatForm</form>
-                <div className={cx('footer')}>
-                    <BsPlusCircleFill />
-                    <PiPaperPlaneTiltFill />
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        className={cx('text-input')}
+                        style={{ height: textareaHeight }}
+                        rows={1}
+                        value={content}
+                        onChange={handleChange}
+                        placeholder="내용을 입력해주세요"
+                    ></textarea>
+                    <div className={cx('footer')}>
+                        <button className={cx('file-btn')}>
+                            <BsPlusCircleFill />
+                        </button>
+                        <button type="submit" className={cx('submit-btn', { active: content })} disabled={!content}>
+                            <PiPaperPlaneTiltFill />
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
