cb02d3f07612722eaac33821e9952ca821deb900
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-19 23:05:12 +0900

    #8 feat: 더미데이터를 이용한 ChatPanel 구현
    - 메세지 불러오기
    - 화면 표시

--- src/pages/Lines/ChatPanel/ChatMessages/ChatMessages.tsx
+++ src/pages/Lines/ChatPanel/ChatMessages/ChatMessages.tsx
@@ -1,5 +1,18 @@
-const ChatMessages = () => {
-    return <div>ChatMessages</div>;
+import { Message } from '@pages/Lines/ChatPanel/ChatPanel';
+
+type PropsType = {
+    message: Message;
+};
+
+const ChatMessages = ({ message }: PropsType) => {
+    return (
+        <div>
+            <div>
+                <strong>{message.user.name}</strong>
+            </div>
+            <p>{message.content}</p>
+        </div>
+    );
 };
 
 export default ChatMessages;
--- src/pages/Lines/ChatPanel/ChatPanel.tsx
+++ src/pages/Lines/ChatPanel/ChatPanel.tsx
@@ -1,12 +1,36 @@
 import ChatForm from '@pages/Lines/ChatPanel/ChatForm/ChatForm';
+import ChatMessages from '@pages/Lines/ChatPanel/ChatMessages/ChatMessages';
 import classnames from 'classnames/bind';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './ChatPanel.module.css';
 const cx = classnames.bind(styles);
 
+export type Message = {
+    id: string;
+    content: string;
+    timeStamp: string;
+    user: {
+        id: string;
+        name: string;
+    };
+};
+
 const ChatPanel = () => {
+    const [messages, setMessages] = useState<Message[]>([]);
+    useEffect(() => {
+        fetch('/data/messages.json')
+            .then((res) => res.json())
+            .then((data) => setMessages(data));
+    }, []);
+
+    const renderMessages = useCallback(() => {
+        if (messages.length <= 0) return;
+        return messages.map((message) => <ChatMessages key={message.id} message={message} />);
+    }, [messages]);
+
     return (
         <div className={cx('container')}>
-            <div className={cx('chat-message-container')}>ChatMessages</div>
+            <div className={cx('chat-message-container')}>{renderMessages()}</div>
             <ChatForm />
         </div>
     );
