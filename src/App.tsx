ed6f1ecb69ccca40a47c18d30627f4cc7ecfeee5
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-20 01:09:19 +0900

    chore: moment 설치 및 App 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Header from '@components/Header/Header';
 import { darkModeAtom } from '@store/atoms/theme';
 import { useAtomValue } from 'jotai';
+import moment from 'moment';
+import 'moment/dist/locale/ko';
 import { Outlet } from 'react-router-dom';
+
+moment.locale('ko');
 
 function App() {
     const darkMode = useAtomValue(darkModeAtom);
