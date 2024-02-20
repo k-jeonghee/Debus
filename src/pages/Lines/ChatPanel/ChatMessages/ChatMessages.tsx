a0504944632de67822a5fe95f86161b1d3cbdee6
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-20 16:40:19 +0900

    #8 feat: 더미데이터를 이용한 ChatMessages UI 로직 구현

--- src/pages/Lines/ChatPanel/ChatMessages/ChatMessages.tsx
+++ src/pages/Lines/ChatPanel/ChatMessages/ChatMessages.tsx
@@ -1,16 +1,31 @@
 import { Message } from '@pages/Lines/ChatPanel/ChatPanel';
+import { authAtom } from '@store/atoms/tempAuth';
+import classnames from 'classnames/bind';
+import { useAtomValue } from 'jotai';
+import moment from 'moment';
+import { useMemo } from 'react';
+import styles from './ChatMessages.module.css';
+const cx = classnames.bind(styles);
 
 type PropsType = {
     message: Message;
 };
 
 const ChatMessages = ({ message }: PropsType) => {
+    const user = useAtomValue(authAtom);
+    const isMyMessage = useMemo(() => user && user.uid === message.user.id, [user, message.user.id]);
+    const timeAgo = moment(message.timeStamp).fromNow();
+
     return (
-        <div>
+        <div className={cx('container', { me: isMyMessage })}>
+            <div className={cx('user-img')}></div>
             <div>
-                <strong>{message.user.name}</strong>
+                <div className={cx('info')}>
+                    <strong className={cx('user-name')}>{message.user.name}</strong>
+                    <span className={cx('timestamp')}>{timeAgo}</span>
+                </div>
+                <p className={cx('content')}>{message.content}</p>
             </div>
-            <p>{message.content}</p>
         </div>
     );
 };
