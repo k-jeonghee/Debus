9767974a71094681753a44cd22cd4317cce879ab
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-02-24 21:53:37 +0900

    #8 style: 타입명 수정

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -2,13 +2,13 @@ import classnames from 'classnames/bind';
 import styles from './Button.module.css';
 const cx = classnames.bind(styles);
 
-type ButtonPropsType = {
+type ButtonProps = {
     text: string;
     variant: string;
     onClick: () => void;
 };
 
-const Button = ({ text, variant, onClick }: ButtonPropsType) => {
+const Button = ({ text, variant, onClick }: ButtonProps) => {
     return (
         <button className={cx(`${variant}`)} onClick={onClick}>
             {text}
