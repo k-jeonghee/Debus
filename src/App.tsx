bc85b1e41da8929a5795445fc59926df9ca656e3
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-03-10 00:32:32 +0900

    #10 refactor: Root 컴포넌트에 적용된 Context Provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import Header from '@components/Header/Header';
 import { darkModeAtom } from '@store/atoms/theme';
 import { useAtomValue } from 'jotai';
 import { Outlet } from 'react-router-dom';
-import { ModalContextProvider } from 'src/context/ModalContext';
 
 function App() {
     const darkMode = useAtomValue(darkModeAtom);
 
     return (
         <div id="app" className={darkMode ? 'dark' : 'light'}>
-            <ModalContextProvider>
-                <Header />
-                <div className="container">
-                    <Outlet />
-                </div>
-            </ModalContextProvider>
+            <Header />
+            <div className="container">
+                <Outlet />
+            </div>
         </div>
     );
 }
