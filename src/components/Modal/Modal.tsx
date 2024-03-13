a9be883938dfe6e39a5c215975f248dcfced6357
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-03-13 22:58:19 +0900

    #12 refactor: type 분리 및 Modal 컴포넌트 props 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,20 +1,19 @@
 import Overlay from '@components/UI/Overlay/Overlay';
 import classnames from 'classnames/bind';
 import { IoIosClose } from 'react-icons/io';
-import { ModalType, useModal } from 'src/context/ModalContext';
+import { ModalProps } from 'src/@types/modal';
+import { ModalType } from 'src/context/ModalContext';
 import styles from './Modal.module.css';
 const cx = classnames.bind(styles);
 
-export type ModalProps = { modal: ModalType };
-
-const Modal = ({ modal }: ModalProps) => {
-    const { closeModal } = useModal();
+const Modal = ({ modal, onClose }: { modal: ModalType } & ModalProps) => {
     const { element, showCloseIcon } = modal;
+
     return (
-        <Overlay onClose={showCloseIcon ? closeModal : undefined}>
+        <Overlay onClose={showCloseIcon ? onClose : undefined}>
             <div className={cx('container')} onClick={(e) => e.stopPropagation()} onKeyDown={() => {}}>
                 {showCloseIcon ? (
-                    <button className={cx('close')} onClick={closeModal}>
+                    <button className={cx('close')} onClick={onClose}>
                         <IoIosClose />
                     </button>
                 ) : null}
