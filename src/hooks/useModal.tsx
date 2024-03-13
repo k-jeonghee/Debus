c7f2bf5a8c1a04742a365348e0b91f177193eb0e
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-03-13 22:56:54 +0900

    #12 feat: useModal 훅 작성 및 hooks 폴더로 분리

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,35 @@
+import Modal from '@components/Modal/Modal';
+import ModalPortal from '@components/Modal/ModalPortal';
+import { useCallback, useContext, useEffect, useId } from 'react';
+import { modalContext } from 'src/context/ModalContext';
+import { assert } from 'src/utils/assert';
+
+export const useModal = () => {
+    const context = useContext(modalContext);
+    assert(context !== null, 'modalContext is null');
+
+    const { modals, open, close } = context;
+    const modalId = useId();
+
+    const openModal = useCallback(
+        (element: JSX.Element, showCloseIcon: boolean = true) => open(element, modalId, showCloseIcon),
+        [modalId, open],
+    );
+
+    const closeModal = useCallback(() => close(modalId), [modalId, close]);
+
+    const renderModal = useCallback(() => {
+        const [modal] = modals.filter((modal) => modal.id === modalId);
+        return (
+            modal && (
+                <ModalPortal>
+                    <Modal modal={modal} onClose={closeModal} />
+                </ModalPortal>
+            )
+        );
+    }, [modals, modalId, closeModal]);
+
+    useEffect(() => closeModal(), [closeModal]);
+
+    return { openModal, closeModal, renderModal };
+};
