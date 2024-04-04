898fe2c6f5b089649ecfc6a459f3cb864f658cd1
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2024-04-04 16:54:55 +0900

    #14 feat: TanStack-Query Client 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Header from '@components/Header/Header';
 import { darkModeAtom } from '@store/atoms/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useAtomValue } from 'jotai';
 import moment from 'moment';
 import 'moment/dist/locale/ko';
@@ -8,17 +10,21 @@ import { ModalProvider } from 'src/context/ModalContext';
 
 moment.locale('ko');
 
+export const queryClient = new QueryClient();
+
 function App() {
     const darkMode = useAtomValue(darkModeAtom);
-
     return (
         <ModalProvider>
-            <div id="app" className={darkMode ? 'dark' : 'light'}>
-                <Header />
-                <div className="container">
-                    <Outlet />
+            <QueryClientProvider client={queryClient}>
+                <div id="app" className={darkMode ? 'dark' : 'light'}>
+                    <Header />
+                    <div className="container">
+                        <Outlet />
+                    </div>
                 </div>
-            </div>
+                <ReactQueryDevtools initialIsOpen={true} />
+            </QueryClientProvider>
         </ModalProvider>
     );
 }
