dc5629a2843ec33433ae5d527685fc1d8c7848b6
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-04-18 16:17:06 +0900

    remove: 이슈 작업 전 폴더 구조 개선

--- src/queries/chatRoom.ts
+++
@@ -1,9 +0,0 @@
-import { createQueryKeys } from '@lukemorales/query-key-factory';
-import { getChatRoom } from 'src/api/firebase';
-
-export const chatRoomsKeys = createQueryKeys('chatRooms', {
-    detail: (chatRoomId: string) => ({
-        queryKey: [chatRoomId],
-        queryFn: () => getChatRoom(chatRoomId),
-    }),
-});
--- src/queries/index.ts
+++
@@ -1,4 +0,0 @@
-import { mergeQueryKeys } from '@lukemorales/query-key-factory';
-import { chatRoomsKeys } from 'src/queries/chatRoom';
-
-export const queryKeys = mergeQueryKeys(chatRoomsKeys);
