9d4d26a94ce06a9726e9d2e3e565f8c33a6786d1
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-04-29 00:39:55 +0900

    #15 refactor: 분리한 queryOptions를 활용해 실시간 메시지 업데이트 로직 개선

--- src/components/Chat/ChatPanel/ChatPanel.tsx
+++ src/components/Chat/ChatPanel/ChatPanel.tsx
@@ -1,27 +1,41 @@
 import ChatForm from '@components/Chat/ChatPanel/ChatForm/ChatForm';
 import ChatMessage from '@components/Chat/ChatPanel/ChatMessage/ChatMessage';
 
-import { useMessageQuery } from '@hooks/services/queries/chat';
+import { messageQueryOptions, useMessage } from '@hooks/services/queries/chat';
+import { keepPreviousData, useQueryClient } from '@tanstack/react-query';
 import classnames from 'classnames/bind';
-import { useEffect } from 'react';
+import { memo, useEffect } from 'react';
 import { addMessageListener } from 'src/api/firebase';
 import styles from './ChatPanel.module.css';
 
 const cx = classnames.bind(styles);
 
-const ChatPanel = ({ id }: { id: string }) => {
-    const { data, refetch } = useMessageQuery(id);
+const ChatPanel = ({ chatRoomId }: { chatRoomId: string }) => {
+  const messages = useMessage(chatRoomId);
+  const queryClient = useQueryClient();
 
-    useEffect(() => addMessageListener(id, () => refetch()), [id, refetch]);
+  useEffect(
+    () =>
+      addMessageListener(chatRoomId, () =>
+        queryClient.refetchQueries(
+          messageQueryOptions(chatRoomId, {
+            placeholderData: keepPreviousData,
+          }),
+        ),
+      ),
+    [chatRoomId, queryClient],
+  );
 
-    const messages$ = data.map((message) => <ChatMessage key={message.id} message={message} />);
-
-    return (
-        <div className={cx('container')}>
-            <div className={cx('chat-message-container')}>{messages$}</div>
-            <ChatForm />
-        </div>
-    );
+  return (
+    <div className={cx('container')}>
+      <div className={cx('chat-message-container')}>
+        {messages.map((message) => (
+          <ChatMessage key={message.id} message={message} />
+        ))}
+      </div>
+      <ChatForm />
+    </div>
+  );
 };
 
-export default ChatPanel;
+export default memo(ChatPanel);
