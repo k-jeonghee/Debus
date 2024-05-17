83d3686d38785e4e192b973889ee3323e68ab259
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:24:09 +0900

    refactor: 전역 모달 렌더링 방식 개선

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,7 +1,7 @@
 import Modal from '@components/@common/Modal/Modal';
 import ModalPortal from '@components/@common/Modal/ModalPortal';
-import { ComponentType, createElement, useCallback, useContext, useEffect, useId } from 'react';
-import { ActionModalProps } from 'src/@types/modal';
+import { ComponentType, useCallback, useContext, useEffect, useId } from 'react';
+import { ActionInfo } from 'src/@types/modal';
 import { modalContext } from 'src/context/ModalContext';
 import { assert } from 'src/utils/assert';
 
@@ -13,10 +13,10 @@ export const useModal = () => {
   const modalId = useId();
   const closeModal = useCallback(() => close(modalId), [modalId, close]);
   const openModal = useCallback(
-    <P extends { onSubmit(value: unknown): unknown }>(Component: ComponentType<P>) =>
+    <P extends { onSubmit(value: unknown): unknown }>(Component: ComponentType<P>, actionInfo?: ActionInfo) =>
       new Promise<Parameters<P['onSubmit']>[0]>((resolve, reject) => {
         const modal = {
-          element: createElement(Component),
+          element: Component,
           modalId,
           resolve: <T extends {}>(value?: T) => {
             resolve(value);
@@ -26,25 +26,9 @@ export const useModal = () => {
             reject(reason);
             closeModal();
           },
-        };
-        open(modal);
-      }),
-    [modalId, open, closeModal],
-  );
-
-  const openActionModal = useCallback(
-    <P extends ActionModalProps>(component: ComponentType<P>, props: P) =>
-      new Promise((resolve, reject) => {
-        const modal = {
-          element: createElement(component, { ...props }),
-          modalId,
-          resolve: <T extends {}>(value?: T) => {
-            resolve(value);
-            closeModal();
-          },
-          reject: (reason?: Error) => {
-            reject(reason);
-            closeModal();
+          actionInfo: actionInfo && {
+            type: actionInfo.type,
+            message: actionInfo.message,
           },
         };
         open(modal);
@@ -64,5 +48,5 @@ export const useModal = () => {
   }, [modals, modalId, portalRef]);
 
   useEffect(() => closeModal, [closeModal]);
-  return { openModal, closeModal, openActionModal, ModalContainer };
+  return { openModal, closeModal, ModalContainer };
 };
