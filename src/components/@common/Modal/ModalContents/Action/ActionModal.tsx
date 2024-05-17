16b7b749d55ea6fe4e06e994868f84cea6b08969
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:25:02 +0900

    refactor: ActionModal 옵셔널 파라미터 객체로 변경

--- src/components/@common/Modal/ModalContents/Action/ActionModal.tsx
+++ src/components/@common/Modal/ModalContents/Action/ActionModal.tsx
@@ -4,29 +4,30 @@ import ModalContent from '@components/@common/Modal/ModalTemplate/ModalContent';
 import ModalHeader from '@components/@common/Modal/ModalTemplate/ModalHeader';
 import ModalTemplate from '@components/@common/Modal/ModalTemplate/ModalTemplate';
 import classnames from 'classnames/bind';
-import { ActionModalProps } from 'src/@types/modal';
+import { ActionModalProps, ActionType } from 'src/@types/modal';
 import styles from './ActionModal.module.css';
 const cx = classnames.bind(styles);
 
-const displayName = {
-    confirm: '확인',
-    delete: '삭제',
+const displayName: Record<ActionType, string> = {
+  confirm: '확인',
+  delete: '삭제',
+  exit: '나가기',
 };
 
-const ActionModal = ({ actionType, desc, onSubmit, onAbort }: ActionModalProps) => {
-    const handleClick = () => onSubmit?.(true);
-    return (
-        <ModalTemplate isOverlay={true} styleType="alert" transparent={true}>
-            <ModalHeader onClose={onAbort} />
-            <ModalContent>
-                <p className={cx('desc')}>{desc}</p>
-                <ModalButtonGroup>
-                    <Button text="취소" variant="default" onClick={onAbort} />
-                    <Button text={displayName[actionType]} variant={actionType} type="submit" onClick={handleClick} />
-                </ModalButtonGroup>
-            </ModalContent>
-        </ModalTemplate>
-    );
+const ActionModal = ({ onSubmit, onAbort, actionInfo }: ActionModalProps<boolean>) => {
+  const handleClick = () => onSubmit(true);
+  return (
+    <ModalTemplate isOverlay={true} styleType="alert" transparent={true}>
+      <ModalHeader onClose={onAbort} />
+      <ModalContent>
+        <p className={cx('message')}>{actionInfo.message}</p>
+        <ModalButtonGroup>
+          <Button text="취소" variant="default" onClick={onAbort} />
+          <Button text={displayName[actionInfo.type]} variant={actionInfo.type} type="submit" onClick={handleClick} />
+        </ModalButtonGroup>
+      </ModalContent>
+    </ModalTemplate>
+  );
 };
 
 export default ActionModal;
