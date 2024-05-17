b698ea0cb10d922a169470032aec08be7f5f97d7
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:19:02 +0900

    #21 feat: 채팅방 나가기 기능 구현

--- src/components/Chat/SidePanel/SidePanel.tsx
+++ src/components/Chat/SidePanel/SidePanel.tsx
@@ -1,20 +1,50 @@
+import Button from '@components/@common/Button/Button';
+import ActionModal from '@components/@common/Modal/ModalContents/Action/ActionModal';
 import ChatMembers from '@components/Chat/SidePanel/ChatMembers/ChatMembers';
 import ChatRoomHeader from '@components/Chat/SidePanel/ChatRoomHeader/ChatRoomHeader';
-
-import { useChatRoom } from '@hooks/services/queries/chat';
+import { useModal } from '@hooks/useModal';
+import { authAtom } from '@store/atoms/auth';
 import classnames from 'classnames/bind';
+import { useAtomValue } from 'jotai';
 import { memo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ChatRoomInfoType } from 'src/@types/chat';
+import { exitChatRoom } from 'src/api/firebase';
 import styles from './SidePanel.module.css';
 const cx = classnames.bind(styles);
 
-const SidePanel = ({ chatRoomId }: { chatRoomId: string }) => {
-  const chatRoom = useChatRoom(chatRoomId);
-  const { members } = chatRoom;
+const SidePanel = ({ chatRoom }: { chatRoom: ChatRoomInfoType }) => {
+  const { uid } = useAtomValue(authAtom);
+  const { openModal, ModalContainer } = useModal();
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      const res = await openModal(ActionModal, { type: 'exit', message: '채팅방에서 나가면 기록을 복구할 수 없어요.' });
+      if (res) {
+        const curMembersCount = chatRoom.members.length;
+        try {
+          await exitChatRoom(uid, chatRoom.id, curMembersCount === 1);
+          navigate(`/`);
+        } catch (err) {
+          //삭제 실패
+          console.log(err);
+        }
+      }
+    } catch (err) {
+      //사용자가 직접 모달을 닫음
+      console.log(err);
+    }
+  };
 
   return (
     <div className={cx('container')}>
-      <ChatRoomHeader chatRoom={chatRoom} />
-      <ChatMembers members={members} />
+      <div>
+        <ChatRoomHeader chatRoom={chatRoom} />
+        <ChatMembers members={chatRoom.members} />
+      </div>
+      <Button text="나가기" variant="accent" onClick={handleClick} />
+      <ModalContainer />
     </div>
   );
 };
