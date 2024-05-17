af09e5983dc94e6e961aee67be94753dcfefd1f7
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:16:45 +0900

    #21 refactor: auth 타입 수정

--- src/store/atoms/auth.ts
+++ src/store/atoms/auth.ts
@@ -1,17 +1,21 @@
 import { atom } from 'jotai';
 import { onUserStateChange } from 'src/api/firebase';
 
-export type UserTypes = {
-    uid: string;
-    displayName: string | null;
-    photoURL: string | null;
-    email: string | null;
-};
+export interface BaseUser {
+  uid: string;
+  displayName: string | null;
+  photoURL: string | null;
+  email: string | null;
+}
 
-export const baseAuthAtom = atom<UserTypes | null>(null);
-export const authAtom = atom<Promise<UserTypes>>((get) => {
-    const x = get(baseAuthAtom);
-    if (!x) return new Promise(() => {});
-    return Promise.resolve(x);
+export interface UserTypes extends BaseUser {
+  chatRooms: { id: string; nickName: string }[];
+}
+
+export const baseAuthAtom = atom<BaseUser | null>(null);
+export const authAtom = atom<Promise<BaseUser>>((get) => {
+  const x = get(baseAuthAtom);
+  if (!x) return new Promise(() => {});
+  return Promise.resolve(x);
 });
 baseAuthAtom.onMount = (set) => onUserStateChange(set);
