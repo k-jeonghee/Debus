44387c11c237c31f87e03d32b59a2acb5ea40539
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:30:04 +0900

    #21 refactor: LinePage Suspense 적용

--- src/router.tsx
+++ src/router.tsx
@@ -1,33 +1,39 @@
+import Loading from '@components/@common/Loading/Loading';
 import HomePage from '@pages/HomePage/HomePage';
 import LinePage from '@pages/LinePage/LinePage';
 import MyPage from '@pages/MyPage/MyPage';
 import OperationPage from '@pages/OperationPage/OperationPage';
 import StationPage from '@pages/StationPage/StationPage';
+import { Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from 'src/App';
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            { index: true, element: <HomePage /> },
-            {
-                path: '/stations',
-                element: <StationPage />,
-            },
-            {
-                path: '/lines/:id',
-                element: <LinePage />,
-            },
-            {
-                path: '/my-page',
-                element: <MyPage />,
-            },
-        ],
-    },
-    {
-        path: '/my-operation',
-        element: <OperationPage />,
-    },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: '/stations',
+        element: <StationPage />,
+      },
+      {
+        path: '/lines/:id',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <LinePage />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/my-page',
+        element: <MyPage />,
+      },
+    ],
+  },
+  {
+    path: '/my-operation',
+    element: <OperationPage />,
+  },
 ]);
