c6e6e4e8050183e4db9fcaced842308684408a23
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-05-17 17:26:57 +0900

    #21 refactor: 메시지 타입에 채팅방 id 추가

--- src/@types/chat.ts
+++ src/@types/chat.ts
@@ -1,34 +1,35 @@
 export type ChatRoomInfo = {
-    title: string;
-    desc: string;
-    options: string;
+  title: string;
+  desc: string;
+  options: string;
 };
 
 type ChatRoomStatus = 'pending' | 'running';
 
 export type ChatRoomInfoType = {
-    id: string;
-    title: string;
-    desc: string;
-    options: string[];
-    createAt: number;
-    members: Member[];
-    status: ChatRoomStatus;
+  id: string;
+  title: string;
+  desc: string;
+  options: string[];
+  createAt: number;
+  members: Member[];
+  status: ChatRoomStatus;
 };
 
 export type Member = {
-    userId: string;
-    name: string;
-    role: string;
+  userId: string;
+  name: string;
+  role: string;
 };
 
 export type Message = {
+  id: string;
+  content?: string;
+  file?: string;
+  timestamp: unknown;
+  user: {
     id: string;
-    content?: string;
-    file?: string;
-    timestamp: unknown;
-    user: {
-        id: string;
-        name: string;
-    };
+    name: string;
+  };
+  chatRoomId: string;
 };
