28110186cbadba0aecb828be6156962de340c21f
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-06 21:43:26 +0900

    fix: ChatPanel memberInfo assert 로직 제거 및 Suspense 적용

--- src/components/Chat/ChatPanel/ChatPanel.tsx
+++ src/components/Chat/ChatPanel/ChatPanel.tsx
@@ -1,26 +1,24 @@
 import ChatForm from '@components/Chat/ChatPanel/ChatForm/ChatForm';
 import ChatMessage from '@components/Chat/ChatPanel/ChatMessage/ChatMessage';
 
-import { messageQueryOptions, useMessage } from '@hooks/services/queries/chat';
+import Loading from '@components/@common/Loading/Loading';
+import { messageQueryOptions, useChatRoomById, useMessage } from '@hooks/services/queries/chat';
 import { authAtom } from '@store/atoms/auth';
 import { keepPreviousData, useQueryClient } from '@tanstack/react-query';
 import classnames from 'classnames/bind';
 import { useAtomValue } from 'jotai';
-import { memo, useEffect } from 'react';
-import { ChatRoomInfoType } from 'src/@types/chat';
+import { Suspense, memo, useEffect } from 'react';
 import { addMessageListener } from 'src/api/firebase';
-import { assert } from 'src/utils/assert';
 import styles from './ChatPanel.module.css';
 
 const cx = classnames.bind(styles);
 
-const ChatPanel = ({ chatRoom }: { chatRoom: ChatRoomInfoType }) => {
-  const { uid } = useAtomValue(authAtom);
-  const { id: chatRoomId, members } = chatRoom;
+const ChatPanel = ({ chatRoomId }: { chatRoomId: string }) => {
+  const { id: uid } = useAtomValue(authAtom);
+  const { members } = useChatRoomById(chatRoomId);
   const messages = useMessage(chatRoomId);
   //접속한 채팅방 멤버 중 로그인 사용자의 정보
-  const memberInfo = members.find((member) => member.userId === uid);
-  assert(memberInfo !== undefined, '채팅방에 존재하지 않는 사용자입니다.');
+  const memberInfo = members && members.find((member) => member.userId === uid);
 
   const queryClient = useQueryClient();
   useEffect(
@@ -36,19 +34,21 @@ const ChatPanel = ({ chatRoom }: { chatRoom: ChatRoomInfoType }) => {
   );
 
   return (
-    <div className={cx('container')}>
-      <div className={cx('chat-message-container')}>
-        {messages.map((message) => (
-          <ChatMessage
-            key={message.id}
-            message={message}
-            chatRoomId={chatRoomId}
-            isMyMessage={uid === message.user.id}
-          />
-        ))}
+    <Suspense fallback={<Loading />}>
+      <div className={cx('container')}>
+        <div className={cx('chat-message-container')}>
+          {messages.map((message) => (
+            <ChatMessage
+              key={message.id}
+              message={message}
+              chatRoomId={chatRoomId}
+              isMyMessage={uid === message.user.id}
+            />
+          ))}
+        </div>
+        {memberInfo && <ChatForm nickName={memberInfo.name} chatRoomId={chatRoomId} />}
       </div>
-      <ChatForm nickName={memberInfo.name} chatRoomId={chatRoomId} />
-    </div>
+    </Suspense>
   );
 };
 
