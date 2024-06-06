fdbe659cfdf9eb58d804edd7e20f34eff888ef22
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-06 21:34:51 +0900

    refactor: auth 타입 수정

--- src/components/Chat/SidePanel/ChatMembers/ChatMembers.tsx
+++ src/components/Chat/SidePanel/ChatMembers/ChatMembers.tsx
@@ -9,22 +9,22 @@ import styles from './ChatMembers.module.css';
 const cx = classnames.bind(styles);
 
 const ChatMembers = ({ members }: { members: Member[] }) => {
-    const { uid } = useAtomValue(authAtom);
-    return (
-        <div className={cx('container')}>
-            <h1>참여 ({members?.length})</h1>
-            <ul>
-                {members &&
-                    members.map((member) => (
-                        <li key={member.userId} className={cx('item')}>
-                            {member.name}
-                            {member.role === 'owner' && <FaCrown className={cx('icon')} />}
-                            {member.userId === uid && <span className={cx('me')}>나</span>}
-                        </li>
-                    ))}
-            </ul>
-        </div>
-    );
+  const { id: uid } = useAtomValue(authAtom);
+  return (
+    <div className={cx('container')}>
+      <h1>참여 ({members?.length})</h1>
+      <ul>
+        {members &&
+          members.map((member) => (
+            <li key={member.userId} className={cx('item')}>
+              {member.name}
+              {member.role === 'owner' && <FaCrown className={cx('icon')} />}
+              {member.userId === uid && <span className={cx('me')}>나</span>}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default memo(ChatMembers);
