896838e9e20260acd3c33682c933b370584ef730
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-09 21:32:22 +0900

    refactor: mutate 타입 수정

--- src/hooks/services/mutations/chat.ts
+++ src/hooks/services/mutations/chat.ts
@@ -4,18 +4,17 @@ import { UseMutationOptions, useMutation, useQueryClient } from '@tanstack/react
 import { ChatRoomInfo, Message } from 'src/@types/chat';
 import { addNewMessage, createChatRoom } from 'src/api/firebase';
 
+type MutateOptionsType<T, U> = UseMutationOptions<T, Error, U>;
+
 export type createChatRoomMutateType = { user: UserTypes; chatRoomInfo: ChatRoomInfo; nickName: string };
-export const useCreateChatRoom = (options?: UseMutationOptions<string, Error, createChatRoomMutateType>) =>
+export const useCreateChatRoom = (options: MutateOptionsType<string, createChatRoomMutateType>) =>
   useMutation({
     mutationFn: createChatRoom,
     ...options,
   });
 
 export type addMessageMutateType = { chatRoomId: string; message: Omit<Message, 'id' | 'timestamp'> };
-export const useAddMessage = (
-  chatRoomId: string,
-  options: UseMutationOptions<Message, Error, addMessageMutateType>,
-) => {
+export const useAddMessage = (chatRoomId: string, options: MutateOptionsType<Message, addMessageMutateType>) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: addNewMessage,
