f9a350f46dc6ec500a212c0fa49a96e1655354e8
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-09 21:37:48 +0900

    refactor: LinePage Errorboundary 및 채팅 멤버 리스너 적용

--- src/pages/LinePage/LinePage.tsx
+++ src/pages/LinePage/LinePage.tsx
@@ -1,8 +1,13 @@
 import ChatPanel from '@components/Chat/ChatPanel/ChatPanel';
 import SidePanel from '@components/Chat/SidePanel/SidePanel';
-import { useChatRoomById } from '@hooks/services/queries/chat';
+import ErrorRedirect from '@components/Error/ErrorRedirect';
+import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary';
+import { chatRoomByIdQueryOptions } from '@hooks/services/queries/chat';
+import { useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
 import classnames from 'classnames/bind';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { updateMemberListener } from 'src/api/firebase';
 import { assert } from 'src/utils/assert';
 import styles from './LinePage.module.css';
 const cx = classnames.bind(styles);
@@ -10,17 +15,24 @@ const cx = classnames.bind(styles);
 const LinePage = () => {
   const { id: chatRoomId } = useParams();
   assert(chatRoomId !== undefined, '채팅방 id가 존재하지 않습니다.');
-  const chatRoom = useChatRoomById(chatRoomId);
+  const { data: chatRoom } = useSuspenseQuery({ ...chatRoomByIdQueryOptions(chatRoomId) });
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    updateMemberListener(chatRoom.id, () => queryClient.refetchQueries(chatRoomByIdQueryOptions(chatRoom.id)));
+  }, [queryClient, chatRoom.id]);
 
   return (
-    <div className={cx('container')}>
-      <div className={cx('side')}>
-        <SidePanel chatRoom={chatRoom} />
+    <ErrorBoundary Fallback={ErrorRedirect}>
+      <div className={cx('container')}>
+        <div className={cx('side')}>
+          <SidePanel chatRoom={chatRoom} />
+        </div>
+        <div className={cx('chat')}>
+          <ChatPanel chatRoomId={chatRoomId} />
+        </div>
       </div>
-      <div className={cx('chat')}>
-        <ChatPanel chatRoom={chatRoom} />
-      </div>
-    </div>
+    </ErrorBoundary>
   );
 };
 
