75a6333b2e27f63c89a1184d6e75aacda53e20ae
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-09 21:37:02 +0900

    refactor: 로그인/로그아웃 a 태그로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,32 +3,40 @@ import ThemeButton from '@components/@common/ThemeButton/ThemeButton';
 import { baseAuthAtom } from '@store/atoms/auth';
 import classnames from 'classnames/bind';
 import { useAtomValue } from 'jotai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { login, logout } from 'src/api/firebase';
 import styles from './Header.module.css';
 const cx = classnames.bind(styles);
 
 const Header = () => {
   const user = useAtomValue(baseAuthAtom);
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    login();
+    navigate('/');
+  };
+
+  const handleLogOut = () => {
+    logout();
+    navigate('/');
+  };
   return (
     <header id={cx('header')}>
       <div className={cx('header')}>
-        <h1 className={cx('header-logo')}>
-          <Link to={'/'}>
-            <h1>DeBus</h1>
-          </Link>
-        </h1>
+        <Link to={'/'}>
+          <h1>DeBus</h1>
+        </Link>
         <div className={cx('header-right')}>
           <ThemeButton />
           {user && <Avatar />}
           {user ? (
-            <button className={cx('btn-login')} onClick={logout}>
-              로그아웃
-            </button>
+            <a onClick={handleLogOut}>
+              <button className={cx('btn-login')}>로그아웃</button>
+            </a>
           ) : (
-            <button className={cx('btn-login')} onClick={login}>
-              로그인
-            </button>
+            <a onClick={handleLogin}>
+              <button className={cx('btn-login')}>로그인</button>
+            </a>
           )}
         </div>
       </div>
