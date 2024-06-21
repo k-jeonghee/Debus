5a8fb8a420e02e48de90f4676a14a75ab9a3e2a5
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-21 18:03:25 +0900

    refactor: Button 컴포넌트 variant 속성 타입 좁히기

--- src/components/@common/Button/Button.tsx
+++ src/components/@common/Button/Button.tsx
@@ -3,18 +3,18 @@ import styles from './Button.module.css';
 const cx = classnames.bind(styles);
 
 type ButtonProps = {
-    text: string;
-    variant: string;
-    onClick?: () => void | Promise<void>;
-    type?: 'button' | 'submit';
+  text: string;
+  variant?: 'default' | 'accent' | 'confirm' | 'delete' | 'exit';
+  onClick?: () => void | Promise<void>;
+  type?: 'button' | 'submit';
 };
 
 const Button = ({ text, variant, onClick, type = 'button' }: ButtonProps) => {
-    return (
-        <button className={cx(`${variant}`)} onClick={onClick} type={type}>
-            {text}
-        </button>
-    );
+  return (
+    <button className={cx(`${variant}`)} onClick={onClick} type={type}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
