6b6a869b655c20505410d6cfc571a00c6819e6ab
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-21 17:55:25 +0900

    refactor: 최상위 container 태그 변경 & 익명 채팅 프로필 기본 이미지 적용

--- src/components/Chat/ChatPanel/ChatMessage/ChatMessage.tsx
+++ src/components/Chat/ChatPanel/ChatMessage/ChatMessage.tsx
@@ -5,6 +5,7 @@ import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { memo } from 'react';
 import { Message } from 'src/@types/chat';
+import Bus from 'src/assets/bus.svg?react';
 import styles from './ChatMessage.module.css';
 const cx = classnames.bind(styles);
 
@@ -21,7 +22,9 @@ const ChatMessage = ({ message, chatRoomId, isMyMessage }: PropsType) => {
 
   return (
     <div className={cx('container', { me: isMyMessage })}>
-      <div className={cx('profile')}></div>
+      <div className={cx('profile')}>
+        <Bus width="20" height="20" fill="#1e2226" />
+      </div>
       <div className={cx('info')}>
         <strong className={cx('user-name')}>{message.user.name}</strong>
         <div className={cx('content')}>
