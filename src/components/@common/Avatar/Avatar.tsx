81dc99f756bc947cf0ca99a21f5cd1069b61f202
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-21 17:38:49 +0900

    #27 feat: 프로필에 드롭다운 적용

--- src/components/@common/Avatar/Avatar.tsx
+++ src/components/@common/Avatar/Avatar.tsx
@@ -1,7 +1,9 @@
+import { Content, Item, Portal, Root, Trigger } from '@radix-ui/react-dropdown-menu';
 import { baseAuthAtom } from '@store/atoms/auth';
 import classnames from 'classnames/bind';
 import { useAtomValue } from 'jotai';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from 'src/api/firebase';
 import Bus from 'src/assets/bus.svg?react';
 import styles from './Avatar.module.css';
 const cx = classnames.bind(styles);
@@ -10,18 +12,34 @@ const Avatar = () => {
   const user = useAtomValue(baseAuthAtom);
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    if (user) {
-      navigate(`/my-operation/${user.id}`);
-    }
-  };
+  const handleLogOut = () => logout().then(() => navigate('/'));
 
   return (
-    <div className={cx('wrapper')}>
-      <a onClick={handleClick}>
-        <Bus width="20" height="20" fill="#ffa16e" />
-      </a>
-    </div>
+    <>
+      <Root>
+        <Trigger asChild>
+          <button className={cx('btn')}>
+            <Bus width="20" height="20" fill="#ffa16e" />
+          </button>
+        </Trigger>
+        <Portal>
+          <Content className={cx('content')} onCloseAutoFocus={(e) => e.preventDefault()}>
+            <Item className={cx('item')} asChild>
+              {user && <Link to={'/my-operation'}>운행홈</Link>}
+            </Item>
+            <Item className={cx('item')} asChild>
+              {user && <Link to={'/my-page'}>마이페이지</Link>}
+            </Item>
+            <Item className={cx('item')} asChild>
+              <button role="button" onClick={handleLogOut}>
+                로그아웃
+              </button>
+            </Item>
+            <span className={cx('polygon')}>▲</span>
+          </Content>
+        </Portal>
+      </Root>
+    </>
   );
 };
 
