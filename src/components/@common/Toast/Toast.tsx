b3449aec3473982143a26566f8cab24f73c8ccbe
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-27 22:13:38 +0900

    refactor: react-icons 컴포넌트로 선언

--- src/components/@common/Toast/Toast.tsx
+++ src/components/@common/Toast/Toast.tsx
@@ -1,18 +1,22 @@
 import { Close, Description, Root } from '@radix-ui/react-toast';
+
 import { GoAlert } from '@react-icons/all-files/go/GoAlert';
 import { GoCheck } from '@react-icons/all-files/go/GoCheck';
 import { ToastType } from 'src/context/ToastContext';
 
 const toastType = {
-  success: <GoCheck />,
-  failure: <GoAlert />,
+  success: GoCheck,
+  failure: GoAlert,
 };
 
 const Toast = ({ id, type, message, duration, onRemove }: ToastType & { onRemove: (toastId: string) => void }) => {
+  const Icon = toastType[type];
   return (
     <Root className={`ToastRoot ${type}`} duration={duration ?? 3000}>
       <Description className="ToastDescription">
-        <span className="toast-icon">{toastType[type]}</span>
+        <span className="toast-icon">
+          <Icon />
+        </span>
         <p>{message}</p>
       </Description>
       <Close className="btn-close" onClick={() => onRemove(id)}>
