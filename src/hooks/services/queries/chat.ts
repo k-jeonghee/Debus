9ce4757ee1df619876c95a20ccc9828af166bd73
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-27 22:21:49 +0900

    refactor: chat query 사용하지 않는 코드 제거 및 key 수정

--- src/hooks/services/queries/chat.ts
+++ src/hooks/services/queries/chat.ts
@@ -1,12 +1,11 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getChatRoom, getChatRooms, getChatRoomsByUser, getMessage, getMessages } from 'src/api/firebase';
+import { getChatRoom, getChatRooms, getChatRoomsByUser, getMessages } from 'src/api/firebase';
 
 export const chatKeys = {
   chatRooms: ['chatRooms'] as const,
-  chatRoomById: (chatRoomId: string) => [...chatKeys.chatRooms, chatRoomId] as const,
+  chatRoomById: (chatRoomId: string) => ['chatRoom', chatRoomId] as const,
   chatRoomsByUser: (userId: string) => [...chatKeys.chatRooms, userId] as const,
   messages: (chatRoomId: string) => [...chatKeys.chatRoomById(chatRoomId), 'messages'] as const,
-  messageById: (chatRoomId: string, messageId: string) => [...chatKeys.messages(chatRoomId), messageId] as const,
 };
 
 export const chatRoomQueryOptions = () =>
@@ -31,12 +30,5 @@ export const messageQueryOptions = (chatRoomId: string) =>
   queryOptions({
     queryKey: chatKeys.messages(chatRoomId),
     queryFn: () => getMessages(chatRoomId),
-    staleTime: 200000,
+    staleTime: 1000 * 60 * 4,
   });
-
-export const messageByIdQueryOptions = (chatRoomId: string, messageId: string) => {
-  return queryOptions({
-    queryKey: chatKeys.messageById(chatRoomId, messageId),
-    queryFn: () => getMessage(chatRoomId, messageId),
-  });
-};
