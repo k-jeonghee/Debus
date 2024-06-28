c0d80d1cf9b67e77e947795c9b567ab6e7f8ac8e
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-06-28 14:12:18 +0900

    fix: 타입 에러 수정

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,37 +1,41 @@
 import { MutableRefObject, PropsWithChildren, createContext, useCallback, useMemo, useRef, useState } from 'react';
-import { ModalType } from 'src/@types/modal';
+import { ModalParameters, ModalType } from 'src/@types/modal';
 
-type ModalContextValue = {
-    open: (modal: ModalType) => void;
-    close: (id: string) => void;
-    modals: ModalType[];
-    portalRef: MutableRefObject<HTMLDivElement | null>;
+type ModalContextValue<P> = {
+  open: (modal: ModalType<P>) => void;
+  close: (id: string) => void;
+  modals: ModalType<P>[];
+  portalRef: MutableRefObject<HTMLDivElement | null>;
 };
 
-export const modalContext = createContext<ModalContextValue | null>(null);
+/**
+ * Note: Provider value Type Error 해결을 위해 any 적용
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const modalContext = createContext<ModalContextValue<any> | null>(null);
 
 export const ModalProvider = ({ children }: PropsWithChildren) => {
-    const [modals, setModals] = useState<ModalType[]>([]);
-    const portalRef = useRef<HTMLDivElement | null>(null);
+  const [modals, setModals] = useState<ModalType<ModalParameters>[]>([]);
+  const portalRef = useRef<HTMLDivElement | null>(null);
 
-    const open = useCallback((modal: ModalType) => setModals((prev) => [...prev, modal]), []);
-    const close = useCallback((id: string) => setModals((prev) => prev.filter((v) => v.modalId !== id)), []);
+  const open = useCallback((modal: ModalType<ModalParameters>) => setModals((prev) => [...prev, modal]), []);
+  const close = useCallback((id: string) => setModals((prev) => prev.filter((v) => v.modalId !== id)), []);
 
-    const value = useMemo(
-        () => ({
-            modals,
-            open,
-            close,
-            portalRef,
-        }),
-        [modals, open, close],
-    );
+  const value = useMemo(
+    () => ({
+      modals,
+      open,
+      close,
+      portalRef,
+    }),
+    [modals, open, close],
+  );
 
-    return (
-        <modalContext.Provider value={value}>
-            <div style={{ position: 'relative' }} ref={portalRef}>
-                {children}
-            </div>
-        </modalContext.Provider>
-    );
+  return (
+    <modalContext.Provider value={value}>
+      <div style={{ position: 'relative' }} ref={portalRef}>
+        {children}
+      </div>
+    </modalContext.Provider>
+  );
 };
