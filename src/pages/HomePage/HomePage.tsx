4d265b1f176a5ef885c04123deda7568d34b5c70
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-23 00:06:14 +0900

    feat: 앱 첫 화면 데모 안내 모달 열기

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,25 @@
+import ActionModal from '@components/@common/Modal/ModalContents/Action/ActionModal';
 import ChatRooms from '@components/Chat/ChatRooms';
+import { useModal } from '@hooks/useModal';
+import { useEffect } from 'react';
 
 const HomePage = () => {
-  return <ChatRooms />;
+  const { ModalContainer, openModal } = useModal();
+
+  useEffect(() => {
+    openModal(ActionModal, {
+      type: 'confirm',
+      message: '로그인 버튼을 누르면 익명 로그인으로 전환되어 별도 가입 없이 테스트가 가능합니다.',
+      displayCancel: false,
+    });
+  }, []);
+
+  return (
+    <>
+      <ChatRooms />
+      <ModalContainer />
+    </>
+  );
 };
 
 export default HomePage;
