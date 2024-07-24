f49de32a4e8a01d441c571625b2d05341fb79542
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-24 20:46:11 +0900

    refactor: Demo 알림 모달 확인 여부를 세션에 저장하여 한번만 뜨도록 수정

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,12 +7,16 @@ const HomePage = () => {
   const { ModalContainer, openModal } = useModal();
 
   useEffect(() => {
-    openModal(ActionModal, {
-      type: 'confirm',
-      message: '로그인 버튼을 누르면 익명 로그인으로 전환되어 별도 가입 없이 테스트가 가능합니다.',
-      displayCancel: false,
-    });
-  }, []);
+    const hasSeenDemoAlert = sessionStorage.getItem('hasSeenDemoAlert');
+    if (!hasSeenDemoAlert) {
+      openModal(ActionModal, {
+        type: 'confirm',
+        message: '로그인 버튼을 누르면 익명 로그인으로 전환되어 별도 가입 없이 테스트가 가능합니다.',
+        displayCancel: false,
+      });
+      sessionStorage.setItem('hasSeenDemoAlert', 'true');
+    }
+  }, [openModal]);
 
   return (
     <>
