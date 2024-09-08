959c4c8c697a7b89b66dbd435439415ca151cc6f
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-08 23:46:12 +0900

    chore: vite config 필요 없는 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: [
-            { find: '@src', replacement: resolve(__dirname, 'src') },
-            {
-                find: '@components',
-                replacement: resolve(__dirname, 'src/components'),
-            },
-        ],
-    },
-    base: './',
-    plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
